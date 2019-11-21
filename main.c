d02fbf09c09deef8110f748a0dd3a3771df2915f
subinjeong123 <55428469+subinjeong123@users.noreply.github.com>
2019-11-21 15:28:55 +0900

    12-5(2)

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
-#include <stdio.h>
-#include <string.h> 
+#include <string.h>
+
 
 
 int main(void) 
@@ -10,12 +10,13 @@ int main(void)
 	char input[100];
 	FILE *fp;
 	
-	fp = fopen("sample.txt", "w");
+	fp = fopen("sample.txt", "r");
 	for(i=0;i<3;i++)
 	{
-		printf("input a word :");
-		scanf("%s", input);
-		fprintf(fp, "%s\n", input);
+		//printf("input a word :");
+		//scanf("%s", input);
+		fprintf(fp, "%s", input);
+		printf("s\n", input);
 	}
 	
 	fclose(fp);
